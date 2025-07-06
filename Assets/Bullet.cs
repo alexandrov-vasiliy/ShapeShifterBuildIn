333d8c1bc78165cdeede9435ea6530113389e086
alexandrov-vasiliy <zowel.vasili@yandex.ru>
2025-07-06 20:19:44 +0300

    frontier

---
+++ Assets/Bullet.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   public ParticleSystem Muzzle;
+   public ParticleSystem Tracer;
+
+   private void Start()
+   {
+      Muzzle.Play();
+      Tracer.Play();
+      Destroy(gameObject, 1f);
+   }
+}
